4e170fa311c73b5bbc77fc6e79d71254338019d5
Claudia <claudiaareneee@gmail.com>
2019-04-24 18:02:49 -0500

    added all report types

--- public/js/demo/new-report.js
+++ public/js/demo/new-report.js
@@ -2,7 +2,8 @@ var reportOps = document.getElementById("new-report-dropdown-ops");
 var reportSLA = document.getElementById("new-report-dropdown-sla");
 var reportRental = document.getElementById("new-report-dropdown-rental");
 var reportGap = document.getElementById("new-report-dropdown-gap");
-var reportRoute = document.getElementById("new-report-dropdown-route");
+var reportRouteVariance = document.getElementById("new-report-dropdown-variance");
+var reportRoutePerformance = document.getElementById("new-report-dropdown-performance");
 var reportVolume = document.getElementById("new-report-dropdown-volume");
 var reportProductivity = document.getElementById("new-report-dropdown-productivty");
 var reportService = document.getElementById("new-report-dropdown-service");
@@ -46,16 +47,80 @@ disableInputs = function(myargs){
     }
 }
 
+enableInputs = function(myargs){
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        
+        // element.setAttribute('disabled', true);
+
+        // if(arguments.includes(element.id)){
+        if(myargs.includes(element.id)){
+            element.removeAttribute('disabled');
+        } else {
+            element.setAttribute('disabled', true);
+        }
+
+    }
+}
+
 reportSLA.addEventListener("click", function(){
+    reportDropdown.innerText = reportSLA.innerText;
     disableInputs([formElementIds.route, formElementIds.service, formElementIds.country, formElementIds.city, formElementIds.state, formElementIds.zip]);
 }, false);
 
 reportOps.addEventListener("click", function(){
+    reportDropdown.innerText = reportOps.innerText;
     disableInputs([formElementIds.manager, formElementIds.route, formElementIds.service, formElementIds.country, formElementIds.city, formElementIds.state, formElementIds.zip]);
 }, false);
 
 reportRental.addEventListener("click", function(){
+    reportDropdown.innerText = reportRental.innerText;
     disableInputs([formElementIds.location, formElementIds.manager, formElementIds.route, formElementIds.service, formElementIds.country, formElementIds.city, formElementIds.state, formElementIds.zip]);
+}, false);
+
+reportGap.addEventListener("click", function(){
+    reportDropdown.innerText = reportGap.innerText;
+    enableInputs([formElementIds.location]);
+}, false);
+
+reportRouteVariance.addEventListener("click", function(){
+    reportDropdown.innerText = reportRouteVariance.innerText;
+    enableInputs([formElementIds.location, formElementIds.start, formElementIds.end, formElementIds.route]);
+}, false);
+
+reportRoutePerformance.addEventListener("click", function(){
+    reportDropdown.innerText = reportRoutePerformance.innerText;
+    enableInputs([formElementIds.location, formElementIds.start, formElementIds.end, formElementIds.route]);
+}, false);
+
+reportVolume.addEventListener("click", function(){
+    reportDropdown.innerText = reportVolume.innerText;
+    enableInputs([formElementIds.division, formElementIds.region, formElementIds.district, formElementIds.location, formElementIds.route]);
+}, false);
+
+reportProductivity.addEventListener("click", function(){
+    reportDropdown.innerText = reportProductivity.innerText;
+    disableInputs([formElementIds.route, formElementIds.service]);
+}, false);
+
+reportService.addEventListener("click", function(){
+    reportDropdown.innerText = reportService.innerText;
+    enableInputs([formElementIds.route, formElementIds.service]);
+}, false);
+
+reportStaffing.addEventListener("click", function(){
+    reportDropdown.innerText = reportStaffing.innerText;
+    enableInputs([formElementIds.location, formElementIds.manager]);
+}, false);
+
+reportEmployee.addEventListener("click", function(){
+    reportDropdown.innerText = reportEmployee.innerText;
+    enableInputs([formElementIds.location, formElementIds.manager]);
+}, false);
+
+reportStops.addEventListener("click", function(){
+    reportDropdown.innerText = reportStops.innerText;
+    enableInputs([formElementIds.location, formElementIds.manager, formElementIds.route]);
 }, false);
 
 

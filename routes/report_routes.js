2485c569c18ffa7fdfb23acbcac93049468b292e
Lauren <laurenporsini@gmail.com>
2019-04-24 21:13:00 -0500

    gets all objects from mongodb collection

--- routes/report_routes.js
+++ routes/report_routes.js
@@ -1,7 +1,96 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
   
-  app.get('/reports/:id', (req, res) => {
+
+//   function handleError(res, reason, message, code) {
+//     console.log("ERROR: " + reason);
+//     res.status(code || 500).json({"error": message});
+//   }
+
+//  /*  "/api/reports"
+//  *    GET: finds all reports
+//  *    POST: creates a new report
+//  */
+//   app.get("/api/reports", function(req, res) {
+//     db.collection('reports').find({}).toArray(function(err, docs) {
+//       if (err) {
+//         handleError(res, err.message, "Failed to get reports.");
+//       } else {
+//         res.status(200).json(docs);
+//       }
+//     });
+//   });
+
+
+//   app.post("/api/reports", function(req, res) {
+//     var newReport = req.body;
+//     newReport.createDate = new Date();
+  
+//     if (!req.body.name) {
+//       handleError(res, "Invalid user input", "Must provide a name.", 400);
+//     } else {
+//       db.collection('reports').insertOne(newReport, function(err, doc) {
+//         if (err) {
+//           handleError(res, err.message, "Failed to create new report.");
+//         } else {
+//           res.status(201).json(doc.ops[0]);
+//         }
+//       });
+//     }
+//   });
+
+
+// /*  "/api/reports/:id"
+//  *    GET: find report by id
+//  *    PUT: update report by id
+//  *    DELETE: deletes report by id
+//  */
+// app.get("/api/reports/:id", function(req, res) {
+//   db.collection('reports').findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get report");
+//     } else {
+//       res.status(200).json(doc);
+//     }
+//   });
+// });
+
+
+// app.put("/api/reports/:id", function(req, res) {
+//   var updateDoc = req.body;
+//   delete updateDoc._id;
+
+//   db.collection('reports').updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to update report");
+//     } else {
+//       updateDoc._id = req.params.id;
+//       res.status(200).json(updateDoc);
+//     }
+//   });
+// });
+
+
+// app.delete("/api/reports/:id", function(req, res) {
+//   db.collection('reports').deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to delete report");
+//     } else {
+//       res.status(200).json(req.params.id);
+//     }
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+    app.get('/reports/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
     db.collection('reports').findOne(details, (err, item) => {
@@ -12,6 +101,7 @@ module.exports = function(app, db) {
       } 
     });
   });
+
 
   app.delete('/reports/:id', (req, res) => {
     const id = req.params.id;

d9e3a9b89ffd69ad7cb424c7e49d1ba6760ea901
Claudia <claudiaareneee@gmail.com>
2019-04-26 11:48:50 -0500

    fixed the import previous report function

--- public/js/demo/new-report.js
+++ public/js/demo/new-report.js
@@ -143,6 +143,7 @@ for (let i = 0; i < reports.length; i++) {
     const element = reports[i];
 
     element.addEventListener('click', function () {
+        cancelImport();
         selectedReportText.innerText = element.innerText;
 
         element.setAttribute('hidden', true);
